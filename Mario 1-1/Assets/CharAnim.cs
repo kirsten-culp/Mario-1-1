49fe22d93f0282f1fcfcdc241012138f2ed2260e
kirsten-culp <43148594+kirsten-culp@users.noreply.github.com>
2018-10-19 21:40:29 -0400

    commit6

--- Mario 1-1/Assets/CharAnim.cs
+++ Mario 1-1/Assets/CharAnim.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CharAnim : MonoBehaviour {
 
+    private Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+            }
+        }
 }
