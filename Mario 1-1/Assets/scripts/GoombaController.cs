5717ed4744d491fb7e7c4ed3f1a193db866cfa8b
kirsten-culp <43148594+kirsten-culp@users.noreply.github.com>
2018-10-20 21:05:49 -0400

    commit8

--- Mario 1-1/Assets/scripts/GoombaController.cs
+++ Mario 1-1/Assets/scripts/GoombaController.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class GoombaController : MonoBehaviour {
     public float speed;
+    public Transform wallHitBox;
+    public float wallHitWidth;
+    public float wallHitHeight;
+    public LayerMask isGround;
+    private bool wallHit;
+    private bool isOnGround;
+
     // Use this for initialization
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+        if (wallHit == true)
+        {
+            speed = speed * -1;
+        }
+    }
+    void Update()
+    {
         transform.Translate(speed * Time.deltaTime, 0, 0);
     }
 }
--- Mario 1-1/Assets/scripts/PlayerController.cs
+++ Mario 1-1/Assets/scripts/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private AudioSource source;
+    public AudioClip jumpClip;
+
+    public AudioClip Pickup_Coin6;
+
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
 
     private Rigidbody2D rb2d;
     private bool facingRight = true;
@@ -17,9 +24,9 @@ public class PlayerController : MonoBehaviour
     public float checkRadius;
     public LayerMask allGround;
 
-    // private float jumpTimeCounter;
-    //public float jumpTime;
-    //private bool isJumping;
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
 
     //audio stuff
 
@@ -30,19 +37,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-
     }
 
     void Awake()
     {
 
-        // source = GetComponent<AudioSource>();
+    source = GetComponent<AudioSource>();
 
     }
 
-    private void Update()
+    void Update()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -51,9 +57,9 @@ public class PlayerController : MonoBehaviour
 
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Vector2 movement = new Vector2(moveHorizontal, 0);
+        Vector2 movement = new Vector2(moveHorizontal, 0);
 
-        // rb2d.AddForce(movement * speed);
+        rb2d.AddForce(movement * speed);
 
         rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
 
@@ -80,6 +86,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("pickUp"))
         {
            other.gameObject.SetActive(false);
+           float vol = Random.Range(volLowRange, volHighRange);
+           source.PlayOneShot(Pickup_Coin6);
         }
     }
 
@@ -100,15 +108,14 @@ public class PlayerController : MonoBehaviour
 
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                // rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+                rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
                 rb2d.velocity = Vector2.up * jumpforce;
-
+                float vol = Random.Range(volLowRange, volHighRange);
+                source.PlayOneShot(jumpClip);
 
                 // Audio stuff
-
-
-
             }
         }
+        
     }
 }
