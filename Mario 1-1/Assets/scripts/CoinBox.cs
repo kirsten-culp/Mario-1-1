f950302ebf2d866d8cbb596bd17f7601e0329907
kirsten-culp <43148594+kirsten-culp@users.noreply.github.com>
2018-10-21 18:05:21 -0400

    commit9

---
+++ Mario 1-1/Assets/scripts/CoinBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBox : MonoBehaviour
+{
+    public LayerMask isGround;
+    public Transform wallHitBox;
+    private bool wallHit;
+    public float wallHitHeight;
+    public float wallHitWidth;
+    public AudioClip Pickup_Coin6;
+    private AudioSource source;
+    public int numberOfCoins;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+        if (wallHit == true)
+        {
+            source.PlayOneShot(Pickup_Coin6);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            source.PlayOneShot(Pickup_Coin6);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallHitBox.position, new Vector3(wallHitWidth, wallHitHeight, 1));
+    }
+}
